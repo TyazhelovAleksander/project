9689526f8909af29fc72541c64e7fc561d0c9a62
Alex <example@mail.ru>
2020-08-28 12:03:10 +0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,16 @@
+"use strict";
+
+// alert('Hello');
+
+// const result = confirm('Are you here?');
+// console.log(result);
+
+// const answer = prompt("Are you 18?", "18");
+// console.log(answer + 5);
+
+const answers = [];
+answers[0] = prompt('What is your name?', '');
+answers[1] = prompt('What is your last name?', '');
+answers[2] = prompt('How old are you?', '');
+
+document.write(answers);
